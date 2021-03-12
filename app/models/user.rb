30ba524f1eb7f9f5e24e3d3b9231b88420c8f21b
Pedro Miranda <pbragamiranda@gmail.com>
2021-03-12 10:25:02 -0300

    sidekiq

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  after_commit :async_update # Run on create & update
+
+  private
+
+  def async_update
+    UpdateUserJob.perform_later(self)
+  end
+end
